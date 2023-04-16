f32e5093b926b03de65e2a58ac868df2ff315597
Shakh7 <00011485wiut@gmail.com>
2023-04-17 04:27:31 +0500

    lead search

--- quotes/serializers.py
+++ quotes/serializers.py
@@ -49,7 +49,7 @@ class QuoteSerializer(serializers.Serializer):
     destination = serializers.SerializerMethodField(method_name='get_destination')
     departure = serializers.SerializerMethodField(method_name='get_departure')
 
-    created_date = serializers.DateField()
+    created_at = serializers.DateField()
 
     def get_customer(self, obj):
         return {
