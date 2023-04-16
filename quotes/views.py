72e5e8fb6e80578b66b522b545548cc2dd0750d5
Shakh7 <00011485wiut@gmail.com>
2023-04-17 03:16:56 +0500

    lead search

--- quotes/views.py
+++ quotes/views.py
@@ -32,7 +32,7 @@ class QuoteListApiView(ListAPIView):
 
         if pick_up_address_query_param:
             queryset = queryset.filter(
-                pick_up_address__city_name__icontains=pick_up_address_query_param
+                Q(pick_up_address__city_name__icontains=pick_up_address_query_param)
             )
 
         if car_query_param:
