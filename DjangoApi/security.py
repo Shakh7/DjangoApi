0ec84050dbab3698c46527d36c1a83a383a1f690
Shakh7 <00011485wiut@gmail.com>
2023-04-22 20:33:28 +0500

    ALLOWED_DOMAINS

--- DjangoApi/security.py
+++ DjangoApi/security.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseForbidden
 
 
 class DomainRestrictionMiddleware:
-    ALLOWED_DOMAINS = ['shipperauto.com', 'api.shipperauto.com', '127.0.0.1:8000']
+    ALLOWED_DOMAINS = ['dashboard.shipperauto.com', 'shipperauto.com', 'api.shipperauto.com', '127.0.0.1:8000']
 
     def __init__(self, get_response):
         self.get_response = get_response
