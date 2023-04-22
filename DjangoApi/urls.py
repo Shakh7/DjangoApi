5652dc148c7c0900be4539df64ff03e0fb84f39c
Shakh7 <00011485wiut@gmail.com>
2023-04-23 01:05:19 +0500

    JWT AUTH SETTINGS

--- DjangoApi/urls.py
+++ DjangoApi/urls.py
@@ -23,7 +23,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
-
+from .views import CustomTokenVerifyView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
@@ -43,7 +43,7 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
