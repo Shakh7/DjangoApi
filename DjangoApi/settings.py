a96c3b35321aa89386e9c2a2f93ffa8b7e7e3ec0
Shakh7 <00011485wiut@gmail.com>
2023-04-22 20:04:04 +0500

    ALLOWED_HOSTS

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rp8%2$i)rvp&a1rpw1m^fk1ceo6)%z0lp-pz+gy)knnlj)b&1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['api.shipperauto.com', '68.183.109.5']
