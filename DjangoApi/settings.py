e25b411ace5fe41800e20f284d36e076eddfc9e3
Shakh7 <00011485wiut@gmail.com>
2023-04-22 19:50:53 +0500

    ALLOWED_HOSTS

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rp8%2$i)rvp&a1rpw1m^fk1ceo6)%z0lp-pz+gy)knnlj)b&1%
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['68.183.109.5']
+ALLOWED_HOSTS = ['https://api.shipperauto.com']
 
 # Application definition
 
