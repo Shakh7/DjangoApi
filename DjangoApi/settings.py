91a29a7b6e0b943a847e90ac0d4de6cfd8fa500e
Shakh7 <00011485wiut@gmail.com>
2023-04-23 00:13:16 +0500

    JWT AUTH SETTINGS

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -54,6 +54,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
+}
 
 ROOT_URLCONF = 'DjangoApi.urls'
 
