d56671cfb6d91583e19b404e0eb523718fa57d70
Shakh7 <00011485wiut@gmail.com>
2023-04-24 05:32:56 +0500

    auth

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -86,28 +86,28 @@ WSGI_APPLICATION = 'DjangoApi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'leads',
+#         'USER': 'shakh',
+#         'PASSWORD': 'ninny2023!',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'leads',
-        'USER': 'shakh',
-        'PASSWORD': 'ninny2023!',
+        'NAME': 'test',
+        'USER': 'dbuser',
+        'PASSWORD': 'cgv7i9rd9d6dv3opos20',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'test',
-#         'USER': 'dbuser',
-#         'PASSWORD': 'cgv7i9rd9d6dv3opos20',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 AUTH_USER_MODEL = "users.CustomUser"
 
