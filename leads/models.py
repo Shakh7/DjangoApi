58b731352b7d007b90d4fd126078500b885d3b61
Shakh7 <00011485wiut@gmail.com>
2023-04-27 19:14:26 +0500

    leads model

--- leads/models.py
+++ leads/models.py
@@ -4,7 +4,7 @@ from users.models import CustomUser
 
 
 class Lead(models.Model):
-    # quote = models.ForeignKey(Quote, on_delete=models.CASCADE, related_name='leads', blank=True, null=True)
+    quote = models.ForeignKey(Quote, on_delete=models.CASCADE, related_name='leads', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     # client = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,7 +13,7 @@ class Lead(models.Model):
     #     unique_together = ('quote', 'client',)
 
     def __str__(self):
-        return self.price
+        return str(self.price)
 
     # @property
     # def get_customer_count(self):
