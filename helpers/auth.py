655b77eda03fc4a33f602ca79c04e3184ab1f158
Shakh7 <00011485wiut@gmail.com>
2023-05-03 17:36:06 +0500

    ALLOWED_HOSTS

--- helpers/auth.py
+++ helpers/auth.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseForbidden
 from rest_framework import generics
 from rest_framework.permissions import BasePermission
 from django.contrib.sessions.models import Session
@@ -12,6 +13,17 @@ class IsAuthenticated(BasePermission):
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.user_type == 'admin'
+
+
+class RefererMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        referer = request.META.get('HTTP_REFERER')
+        if not referer or not referer.startswith('https://app.shipperauto.com'):
+            return HttpResponseForbidden()
+        return self.get_response(request)
 
 
 class SessionAuthAPIListView(generics.ListAPIView):
