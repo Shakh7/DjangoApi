170aa483a5b217583359dcc1827ba6f64b836639
Shakh7 <00011485wiut@gmail.com>
2023-05-03 18:00:51 +0500

    HttpResponse 403

--- helpers/auth.py
+++ helpers/auth.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.permissions import BasePermission
 from django.contrib.sessions.models import Session
@@ -22,7 +22,7 @@ class RefererMiddleware:
     def __call__(self, request):
         referer = request.META.get('HTTP_REFERER')
         if not referer or not referer.startswith('https://app.shipperauto.com'):
-            return JsonResponse('Access Denied', status=403)
+            return HttpResponse('Access Denied', status=403)
         return self.get_response(request)
 
 
