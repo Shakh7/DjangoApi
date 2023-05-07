5a060fb33ab13f9cb9815259bbaf20ca5dfc88bd
Shakh7 <00011485wiut@gmail.com>
2023-05-07 17:04:57 +0500

    redis location

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -82,7 +82,7 @@ SIMPLE_JWT = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
