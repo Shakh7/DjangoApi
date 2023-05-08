980930e392ae491699d66becabba2f3f71a4e9ed
Shakh7 <00011485wiut@gmail.com>
2023-05-08 15:33:42 +0500

    redis checking

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -64,7 +64,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
@@ -100,6 +99,8 @@ WSGI_APPLICATION = 'DjangoApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 #
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
 
 if DEBUG:
     DATABASES = {
@@ -131,10 +132,6 @@ else:
             },
         },
     }
-
-    SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-    SESSION_CACHE_ALIAS = 'default'
-
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
