821b4fe54b55b4431c25b5a565d18c6dec845cdf
Shakh7 <00011485wiut@gmail.com>
2023-05-11 01:28:36 +0500

    settings

--- DjangoApi/urls.py
+++ DjangoApi/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
 
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
-]
 
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls))
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
