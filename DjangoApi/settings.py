3db7c1e54f195ec03ee7eb767c3ad05609e90aa3
Shakh7 <00011485wiut@gmail.com>
2023-06-06 14:04:55 +0500

    DEBUG

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rp8%2$i)rvp&a1rpw1m^fk1ceo6)%z0lp-pz+gy)knnlj)b&1%'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'api.shipperauto.com', '127.0.0.1'
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'DjangoApi.wsgi.application'
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
 
-if DEBUG:
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
