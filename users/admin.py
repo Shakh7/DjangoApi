8fc1173d0a094e2161ae88d1b65d930196411cac
Shakh7 <00011485wiut@gmail.com>
2023-06-06 17:05:46 +0500

    CustomUserAdmin

--- users/admin.py
+++ users/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 from .models import CustomUser as Users
 
+
 # Register your models here.
 
 class CustomUserAdmin(admin.ModelAdmin):
     model = Users
-    list_display = ['first_name', 'last_name', 'email', 'user_type']
+    list_display = ['id', 'first_name', 'last_name', 'email', 'user_type']
 
 
 admin.site.register(Users, CustomUserAdmin)
