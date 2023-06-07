2f77c80775b471cd0f5020052caa60489ba56c19
Shakh7 <00011485wiut@gmail.com>
2023-06-07 14:42:04 +0500

    DEBUG

--- users/managers.py
+++ users/managers.py
@@ -8,14 +8,13 @@ class CustomUserManager(BaseUserManager):
     for authentication instead of usernames.
     """
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, password, **extra_fields):
         """
         Create and save a user with the given email and password.
         """
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        if not username:
+            raise ValueError(_("The Username must be set"))
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save()
         return user
