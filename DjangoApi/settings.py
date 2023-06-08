1d3c182ce5e07861f36d72a9acd9c035b563ba0e
Shakh7 <00011485wiut@gmail.com>
2023-06-08 09:48:17 +0500

    settings

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -58,7 +58,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 50,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
